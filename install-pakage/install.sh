eb58a480794cc0680f07a0e3979dd882ade21c9a
glukhovsa <stepa.gl@yandex.ru>
2025-04-05 11:57:23 +0300

    bash

--- install-pakage/install.sh
+++ install-pakage/install.sh
@@ -33,5 +33,32 @@ echo '' >> installing_log.txt
 
 rm log_temp.txt
 
-echo "------------------- Вывод ошибок установки ---------------------"
-cat installing_log.txt | grep ORA-
+clear
+
+echo "----------------- Вывод ошибок развёртывания -------------------"
+echo "" > errors.txt
+count=$(grep -c . installing_log.txt)
+for (( i=1; i <= $count; i++ ))
+do
+    line=$(head -$i installing_log.txt | tail -1)
+    if [[ $line == *"ORA"* ]]; then
+        echo " " >> errors.txt
+        for (( a=i-3; a <= i; a++))
+        do
+            head -$a installing_log.txt | tail -1 >> errors.txt
+        done
+    fi
+done
+
+size=$(ls -lah errors.txt |cut -d ' ' -f 5)
+if [ $size == 1 ];  
+then
+    echo "Ошибок при развёртывании не было обнаружено"
+    rm errors.txt
+else
+    cat errors.txt | grep ORA
+    echo "За подробностями обращайтесь в файл errors.txt"
+    echo "Если вы разварачивайте эту БД в первый развозможен вывод ошибок удаления элементов." >> errors.txt
+    echo "В этом случае разверните БД ещё раз." >> errors.txt
+fi
+echo "----------------------------------------------------------------"
