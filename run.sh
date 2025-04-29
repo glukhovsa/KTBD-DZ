d0f27ea2e3d22ac14b69a5f9d9281199e8b257f6
BorisKaryshev <boris.karyshev@gmail.com>
2025-04-29 20:42:11 +0300

    [FIRMA-00001] Updated run script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 set -eou pipefail
 set -x
 
-rm -rf KTBD-DZ
+rm -rf KTBD-DZ || true
 git clone https://github.com/glukhovsa/KTBD-DZ
 cd KTBD-DZ
 
 mkdir data && chmod a+rwx data
 
-docker rm ais_sql_server.g ais.g
+docker rm ais_sql_server.g ais.g || true
 docker compose up --build --force-recreate
